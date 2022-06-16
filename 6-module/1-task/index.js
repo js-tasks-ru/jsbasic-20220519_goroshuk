f43bfd6ace577d9838be7428484d4cd3d5a62419
Dmitrii Goroshchuk <goroshuk@yandex.ru>
2022-06-16 19:54:27 +0300

    task6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,53 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+let rows = [
+  {
+    name: "Ilia",
+    age: 25,
+    salary: 1000,
+    city: "Petrozavodsk",
+  },
+  {
+    name: "Vasya",
+    age: 14,
+    salary: 1500,
+    city: "Moscow",
+  },
+  {
+    name: "Ivan",
+    age: 22,
+    salary: 100,
+    city: "Bryansk",
+  },
+  {
+    name: "Petya",
+    age: 45,
+    salary: 990,
+    city: "Chita",
+  },
+];
+
 export default class UserTable {
-  constructor(rows) {
+  #name = "name";
+  #age = "age";
+  #salary = "salary";
+  #city = "city";
+
+  elem = document.body
+    .querySelector("table")
+    .insertAdjacentHTML("afterBegin", this.#template());
+
+  #template() {
+    return `
+    <table>
+      <thead>
+        <tr>
+          <th>"Имя"</th><th>"Возраст"</th><th>"Зарплата"</th><th>"Город"</th>
+        </tr>
+      </thead>
+    </table> `;
   }
+  // constructor(rows) {
+  // this.name = rows;
+  // }
 }
+
+console.log(rows.length);
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,63 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
+  #name = "";
+  #price = "";
+  #category = "";
+  #image = "";
+  #id = "";
+  #parent = document.querySelector("#holder");
+
   constructor(product) {
+    this.#name = product.name;
+    this.#price = product.price.toFixed(2);
+    this.#category = product.category;
+    this.#image = product.image;
+    this.#id = product.id;
+    this.elem = this.#myCard();
+    this.elem
+      .querySelector(".card__button")
+      .addEventListener("click", this.#onButtonClick);
   }
+
+  #myCard() {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${
+            this.#image
+          }" class="card__image" alt="product">
+          <span class="card__price">€${this.#price}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.#name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>`;
+    return div.firstElementChild;
+  }
+
+  get #cardSelector() {
+    return this.#parent.querySelector(".card");
+  }
+
+  get #button() {
+    return this.cardSelector.querySelector(".card__button");
+  }
+
+  #onButtonClick = () => {
+    // alert("click");
+
+    const newEvent = new CustomEvent("product-add", {
+      detail: this.#id,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(newEvent);
+
+    this.elem.addEventListener("product-add", (event) => console.log(event));
+  };
 }
