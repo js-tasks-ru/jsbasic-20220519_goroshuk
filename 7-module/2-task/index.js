4911f3761e9782679df40100cb2118e2a0d4eb12
Dmitrii Goroshchuk <goroshuk@yandex.ru>
2022-06-21 15:08:40 +0300

    task7-not-finished

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,109 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
+  modal = null;
+
   constructor() {
+    this.modal = createElement(`
+    <div class="modal">
+    <div class="modal__overlay"></div>
+  
+      <div class="modal__inner">
+        <div class="modal__header">
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+  
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+  
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>
+  
+    </div>`);
+    this.modal.addEventListener(
+      "click",
+      () => {
+        const target = event.target;
+
+        if (target.closest(".modal__close")) {
+          document.body.classList.remove("is-modal-open");
+
+          document.body.querySelector(".modal").remove();
+        }
+      },
+      {
+        once: true,
+      }
+    );
+    this.getEventListeners = this.#includeEventListeners();
+  }
+
+  setBody(modalBody) {
+    const body = this.modal.querySelector(".modal__body");
+    // console.log(modalBody);
+    body.innerHTML = "";
+
+    return body.append(modalBody);
+  }
+
+  setTitle(title) {
+    const modalTitle = this.modal.querySelector(".modal__title");
+    modalTitle.innerHTML = "";
+
+    return modalTitle.append(title);
+  }
+
+  open() {
+    document.body.classList.add("is-modal-open");
+    document.body.append(this.modal);
+  }
+
+  close() {
+    document.body.classList.remove("is-modal-open");
+
+    // document.body.querySelector(".modal").remove();
+  }
+
+  #includeEventListeners() {
+    document.addEventListener(
+      "keydown",
+      () => {
+        const code = event.code;
+
+        if (code == "Escape") {
+          document.body.classList.remove("is-modal-open");
+
+          document.querySelector(".modal").remove();
+        }
+      },
+      {
+        once: true,
+      }
+    );
+  }
+
+  #closeModalByEscape() {
+    const code = event.code;
+
+    if (code == "Escape") {
+      document.body.classList.remove("is-modal-open");
+
+      document.querySelector(".modal").remove();
+    }
+  }
+
+  #closeModalByButtonFunction() {
+    const target = event.target;
+
+    if (target.closest(".modal__close")) {
+      document.body.classList.remove("is-modal-open");
+
+      target.closest(".modal").remove();
+    }
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,139 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class StepSlider {
+  #stepsAmount = "";
+
   constructor({ steps, value = 0 }) {
+    this.elem = createElement(`
+    <div class="slider">
+
+      <!--Ползунок слайдера с активным значением-->
+      <div class="slider__thumb" style="left: 50%;">
+        <span class="slider__value">2</span>
+      </div>
+
+      <!--Заполненная часть слайдера-->
+      <div class="slider__progress" style="width: 50%;"></div>
+
+      <!--Шаги слайдера-->
+      <div class="slider__steps">
+      </div>
+  </div>`);
+    this.#stepsAmount = steps;
+    this.spanAdder = this.#spanAdder();
+    this.elem
+      .querySelector(".slider__steps > span")
+      .classList.add("slider__step-active");
+    this.eventListenersOuter = this.#eventListenersInner();
   }
+
+  get #sliderGetter() {
+    return document.body.querySelector(".slider");
+  }
+
+  get #sliderProgressGetter() {
+    return this.#sliderGetter.querySelector(".slider__progress");
+  }
+
+  get #sliderThumbGetter() {
+    return this.#sliderGetter.querySelector(".slider__thumb");
+  }
+
+  get #sliderStepsGetter() {
+    return this.#sliderGetter.querySelector(".slider__steps");
+  }
+
+  #eventListenersInner() {
+    this.elem.addEventListener("click", this.#spiceChangerOnClickFunction);
+  }
+
+  #scaleChangerInnerFunction = () => {
+    const sliderProgressSelector = this.#sliderProgressGetter;
+    const sliderThumbSelector = this.#sliderThumbGetter;
+    const sliderCoords = this.#sliderGetter.getBoundingClientRect();
+    const sliderWidth = this.#sliderGetter.clientWidth;
+    const clickCoordsX = event.x - sliderCoords.x;
+    const calculatedIntegerNumber = Math.round(
+      (clickCoordsX / sliderWidth) * (this.#stepsAmount - 1)
+    );
+
+    const scaleDivision = 100 / (this.#stepsAmount - 1);
+
+    sliderThumbSelector.style.left = `${
+      scaleDivision * calculatedIntegerNumber
+    }%`;
+    sliderProgressSelector.style.width = `${
+      scaleDivision * calculatedIntegerNumber
+    }%`;
+  };
+
+  #valueChangerFunction = () => {
+    const sliderCoords = this.#sliderGetter.getBoundingClientRect();
+    const sliderWidth = this.#sliderGetter.clientWidth;
+    const clickCoordsX = event.x - sliderCoords.x;
+    const calculatedIntegerNumber = Math.round(
+      (clickCoordsX / sliderWidth) * (this.#stepsAmount - 1)
+    );
+
+    const sliderValueSelector =
+      this.#sliderGetter.querySelector(".slider__value");
+
+    sliderValueSelector.innerHTML = `${calculatedIntegerNumber}`;
+  };
+
+  #addActiveClassFunction = () => {
+    const sliderCoords = this.#sliderGetter.getBoundingClientRect();
+    const sliderWidth = this.#sliderGetter.clientWidth;
+    const clickCoordsX = event.x - sliderCoords.x;
+    const calculatedIntegerNumber = Math.round(
+      (clickCoordsX / sliderWidth) * (this.#stepsAmount - 1)
+    );
+
+    const stepActive = this.#sliderStepsGetter.querySelector(
+      ".slider__step-active"
+    );
+
+    const allSpansSelector = this.#sliderStepsGetter.querySelectorAll("span");
+
+    stepActive.classList.remove("slider__step-active");
+
+    allSpansSelector[calculatedIntegerNumber].classList.add(
+      "slider__step-active"
+    );
+  };
+
+  #customDispatchEventFunction = () => {
+    const sliderCoords = this.#sliderGetter.getBoundingClientRect();
+    const sliderWidth = this.#sliderGetter.clientWidth;
+    const clickCoordsX = event.x - sliderCoords.x;
+    const calculatedIntegerNumber = Math.round(
+      (clickCoordsX / sliderWidth) * (this.#stepsAmount - 1)
+    );
+
+    const newEvent = new CustomEvent("slider-change", {
+      detail: calculatedIntegerNumber,
+      bubbles: true,
+    });
+
+    this.#sliderGetter.dispatchEvent(newEvent);
+  };
+
+  #spiceChangerOnClickFunction = () => {
+    // individual function below
+    this.#scaleChangerInnerFunction();
+    // individual function below
+    this.#valueChangerFunction();
+    // individual function below
+    this.#addActiveClassFunction();
+    // individual function below
+    this.#customDispatchEventFunction();
+  };
+
+  #spanAdder = () => {
+    let slider = this.elem.querySelector(".slider__steps");
+
+    for (let i = 0; i < this.#stepsAmount; i++) {
+      slider.innerHTML += "<span></span>";
+    }
+  };
 }
